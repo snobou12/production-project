6c3645c728e5318a7890995ac014c33dd9f1e42b
snobou12 <snobou12@yandex.ru>
2023-05-31 04:50:01 +0300

    fix

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,16 +6,15 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        // Создаем экзмпеляры класса через new, чтобы был обьект
-        // HTMLWebpackPlugin - создает HTML-файл для нашего приложения и автоматически внедряет в этот файл все созданные вами пакеты
-        // new webpack.ProgressPlugin - показывает сколько процентов сборки произшло
-        // MiniCssExtractPlugin - из js-scss,css в css
-        // webpack.DefinePlugin - можем env передавать на клиент(в приложение)
-        // webpack.HotModuleReplacementPlugin - чтобы не обновлялась страница при не серьезных изменениях
-        // ReactRefreshWebpackPlugin дополнение к выше плагину для React
-        // BundleAnalyzerPlugin анализируем бандл(очень удобно)
-
+    // Создаем экзмпеляры класса через new, чтобы был обьект
+    // HTMLWebpackPlugin - создает HTML-файл для нашего приложения и автоматически внедряет в этот файл все созданные вами пакеты
+    // new webpack.ProgressPlugin - показывает сколько процентов сборки произшло
+    // MiniCssExtractPlugin - из js-scss,css в css
+    // webpack.DefinePlugin - можем env передавать на клиент(в приложение)
+    // webpack.HotModuleReplacementPlugin - чтобы не обновлялась страница при не серьезных изменениях
+    // ReactRefreshWebpackPlugin дополнение к выше плагину для React
+    // BundleAnalyzerPlugin анализируем бандл(очень удобно), но для ci/cd отключать, иначе infinity загрузка при билде
+    const plugins = [
         new HTMLWebpackPlugin({
             // создаем шаблон
             template: paths.html,
@@ -28,10 +27,13 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new ReactRefreshWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
